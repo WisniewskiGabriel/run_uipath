96afcdeea20ecfba327c7ad4f6424de8daa0894d
WisniewskiGabriel <gabwisniewski@gmail.com>
2023-11-28 22:42:58 -0800

    fix nessa bosta

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import subprocess
 import logging
 import os
 
-username = os.environ.get('USERNAME')
+logging.warning('STARTANDO')
 
-logging.warning('User: '+username)
-
-command = r'C:\\Users\\'+username+'\\AppData\\Local\\Programs\\UiPath\\Studio\\UiRobot.exe execute --process teste_run_with_message_box'
+command = r'C:\\Users\\User\\AppData\\Local\\Programs\\UiPath\\Studio\\UiRobot.exe execute --process teste_run_with_message_box'
 subprocess.run(command, shell=True)
