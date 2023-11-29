b7262d572af59a5c0217d7636ec9735442b30144
WisniewskiGabriel <gabwisniewski@gmail.com>
2023-11-28 21:39:10 -0800

    fix user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import subprocess
 
-command = r'C:\Users\wisni\AppData\Local\Programs\UiPath\Studio\UiRobot.exe execute --process teste_run_with_message_box'
+command = r'C:\Users\User\AppData\Local\Programs\UiPath\Studio\UiRobot.exe execute --process teste_run_with_message_box'
 subprocess.run(command, shell=True)
