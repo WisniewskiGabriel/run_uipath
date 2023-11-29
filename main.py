d391450d8b9c0d45589eed3b2de88f131275d7ea
WisniewskiGabriel <gabwisniewski@gmail.com>
2023-11-28 21:50:08 -0800

    add some logging kind of

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import subprocess
+import logging
+import os
 
-command = r'C:\Users\User\AppData\Local\Programs\UiPath\Studio\UiRobot.exe execute --process teste_run_with_message_box'
+username = os.environ.get('USERNAME')
+
+logging.warning('User: '+username)
+
+command = r'C:\\Users\\'+username+'\\AppData\\Local\\Programs\\UiPath\\Studio\\UiRobot.exe execute --process teste_run_with_message_box'
 subprocess.run(command, shell=True)
