a5180c7e268e474cb335cdd9637fab9efdeb2de3
WisniewskiGabriel <gabwisniewski@gmail.com>
2023-11-28 22:53:14 -0800

    fix?

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ import os
 
 logging.warning('STARTANDO')
 
-command = r'C:\\Users\\User\\AppData\\Local\\Programs\\UiPath\\Studio\\UiRobot.exe execute --process teste_run_with_message_box'
-subprocess.run(command, shell=True)
+command1 = r'cd C:\tools\jenkins\workspace\teste_python'
+command2 = r'C:\Users\User\AppData\Local\Programs\UiPath\Studio\UiRobot.exe execute -p teste_run_with_message_box'
+subprocess.run(command1, shell=True)
+subprocess.run(command2, shell=True)
